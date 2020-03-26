1d5d3ae7cdecc8e4d91f110682bcd5caff28b055
csaw9279 <ayhanxkosan@gmail.com>
2020-03-26 16:58:24 +0100

    updated

--- javascript/hello.js
+++ javascript/hello.js
@@ -1,45 +1,54 @@
-// alert("hello world");
+// alert("Hello world");
 
 // let message = "Hello world";
 // alert(message);
 // message = "Hallo Welt";
 // alert(message);
 
-const LINK_COLOR = "aff0000";
+const LINK_COLOR = "#ff0000";
 console.log("Link bitte in der Farbe ", LINK_COLOR);
+
+// numerische Variable
 
 let highscore = 520233;
 console.log(highscore / 10);
 
+// Zeichenketten/Strings
+
 let firstname = "John";
-let lastname = "Smith";
+let lastname = 'Smith';
 console.log("Name: ", firstname, lastname);
 
 let fullname = 'Jeffrey "The Dude" Lebowski';
 console.log(fullname);
 
-let template = "Dein Highscore ${highscore} Punkte";
+let template = `Dein Highscore sind ${highscore} Punkte`;
 console.log(template);
 
-let isover18 = true;
-console.log(isover18);
+let isOver18 = true;
+console.log(isOver18);
 
 let age = 19;
-console.log("über 18=", age > 18);
+console.log("über 18?", age > 18);
 
-let participants = ["John", "Jane", "Max"];
+// Arrays/Listen
+
+let participants = [ "John", "Jane", "Max" ];
 console.log(participants);
 console.log("Einträge im Array: ", participants.length);
-console.log(participants[1]);
+console.log(participants[2]);
 
-let gameHighscores = [2099, 3010, 3333, 5000];
+let gameHighscores = [2099, 3010, 3333, 5000 ];
 console.log(gameHighscores);
+
+// Objekte
 
 let user = {
     firstname: "John",
     lastname: "Smith",
     age: 25
 };
+
 console.log(user);
 console.log(user.firstname);
 user.highscore = 200;
@@ -48,28 +57,30 @@ console.log(user);
 
 let a = 2;
 let b = 4;
-console.log(a + b);
-console.log(b / (a - 1));
+console.log(a+b);
+console.log(b/(a-1));
 a++;
 console.log(a);
 
-// let myAge = prompt("Wie alt bist du");
-// console.log('Du bis ${myAge} Jahre alt.');
-// console.log('über 18? , myAge > 18');
+// if-Abfrage
+
+// let myAge = prompt("Wie alt bist du?");
+// console.log(`Du bist ${myAge} Jahre alt.`);
+// console.log(`über 18? ${myAge > 18}`);
 
 // if (myAge > 18) {
-// console.log("Glückwunsch über 18");
-// } else{
-// console.log("Leider unter 18");
+//     console.log("Glückwunsch über 18");
+// } else {
+//     console.log("Leider unter 18");
 // }
-
 
 // Schleifen: for Schleife
 
-for (let i = 0; i<10; i++) {
-    console.log('Schleife $(i)');
+for (let i=0; i<10; i++) {
+    console.log(`Schleife ${i}`);
 }
-for (let index = 0; participants < Array.length; j++) {
-    const participants = participants[j];
-    console.log('Teilnehmer*in ${participant}');
+
+for (let j = 0; j < participants.length; j++) {
+    const participant = participants[j];
+    console.log(`Teilnehmer*in ${j} ${participant}`);
 }
