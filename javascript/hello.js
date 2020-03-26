69b5e80e147edcd87a6579d1894ab4de09a0dbc3
csaw9279 <ayhanxkosan@gmail.com>
2020-03-26 16:43:44 +0100

    a

--- javascript/hello.js
+++ javascript/hello.js
@@ -1,2 +1,25 @@
 // alert("hello world");
 
+// let message = "Hello world";
+// alert(message);
+// message = "Hallo Welt";
+// alert(message);
+
+const LINK_COLOR = "aff0000";
+console.log("Link bitte in der Farbe ", LINK_COLOR);
+
+let highscore = 520233;
+console.log(highscore / 10);
+
+let firstname = "John";
+let lastname = "Smith";
+console.log("Name: ", firstname, lastname);
+
+let fullname = 'Jeffrey "The Dude" Lebowski';
+console.log(fullname);
+
+let template = "Dein Highscore ${highscore} Punkte";
+console.log(template);
+
+let isover18 = true;
+console.log(isover18);
