2c7deaf0c3b713f3b1b876a21dc8f0dcf4341caa
csaw9279 <ayhanxkosan@gmail.com>
2020-04-02 17:41:36 +0200

    testwith lecturecode

--- world/main.js
+++ world/main.js
@@ -1,5 +1,5 @@
-let startLayer = L.tileLayer.provider("OpenTopoMap");
 
+let startLayer = L.tileLayer.provider("Esri.WorldTopoMap");
 
 let map = L.map("map", {
     center: [0,0],
@@ -10,26 +10,35 @@ let map = L.map("map", {
 });
 
 L.control.layers({
-    "OpenTopoMap": startLayer,
-    "OpenStreetMap.Mapnik": L.tileLayer.provider("OpenStreetMap.Mapnik"),
-    "OpenStreetMap.BZH": L.tileLayer.provider("OpenStreetMap.BZH"),
+    "OpenTopoMap" : L.tileLayer.provider("OpenTopoMap"),
+    "OpenStreetMap.Mapnik" : L.tileLayer.provider("OpenStreetMap.Mapnik"),
+    "Stamen.TonerLite" : L.tileLayer.provider("Stamen.TonerLite"),
+    "Stamen.Watercolor" : L.tileLayer.provider("Stamen.Watercolor"),
     "Stamen.Terrain" : L.tileLayer.provider("Stamen.Terrain"),
-    "Esri.WorldStreetMap" : L.tileLayer.provider("Esri.WorldStreetMap"),
-
-
+    "Stamen.TerrainBackground" : L.tileLayer.provider("Stamen.TerrainBackground"),
+    "Esri.WorldStreetMap": L.tileLayer.provider("Esri.WorldStreetMap"),
+    "Esri.WorldTopoMap": startLayer,
+    "Esri.WorldImagery": L.tileLayer.provider("Esri.WorldImagery"),
+    "Esri.WorldPhysical": L.tileLayer.provider("Esri.WorldPhysical"),
+    "Esri.WorldGrayCanvas": L.tileLayer.provider("Esri.WorldGrayCanvas"),
+    "CartoDB.Positron": L.tileLayer.provider("CartoDB.Positron")
 }).addTo(map);
 
 //console.log(CONFIRMED);
 for (let i = 1; i < CONFIRMED.length; i++) {
     let row = CONFIRMED[i];
     //console.log(row[2],row[3]);
+    let reg = `${row[0]} ${row[1]}`
+    let lat = row[2];
+    let lng = row[3];
     let val = row[row.length-1];
     let mrk = L.marker([row[2],row[3]]).addTo(map);
-    mrk.bindPopup(`${row[0]} ${row[1]}: ${val}`);
+    mrk.bindPopup(`${reg}: ${val}`);
 
     let circle = L.circleMarker([row[2],row[3]).addTo(map);
+    circle.bindPopup(`${reg}: ${val}`);
+    
 }
-
 
 
 
