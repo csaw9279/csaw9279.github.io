6d9c587a03c2632e364a2c2e005fc7b1176dbedc
csaw9279 <ayhanxkosan@gmail.com>
2020-04-02 10:28:08 +0200

    in js leaflet easy guide

--- nz/main.js
+++ nz/main.js
@@ -1,1 +1,17 @@
 // alert("Hallo Welt")
+
+
+let map = document.querySelector("#map")
+
+// from leaflet coordinates for pancake rocks?!
+
+var mymap = L.map(map).setView([42.1084 , 171.3363], 13);
+
+
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 17,
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+}).addTo(mymap);
+
+
+
