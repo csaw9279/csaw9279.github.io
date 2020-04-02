325c4826cb0d14e479a2b74821a6bc1430fe74bf
csaw9279 <ayhanxkosan@gmail.com>
2020-04-02 10:30:46 +0200

    checkup

--- nz/main.js
+++ nz/main.js
@@ -13,5 +13,8 @@ L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 }).addTo(mymap);
 
+// marks location on set location. also able to create a line or polygon object
 
+var marker = L.marker([42.1084 , 171.3363]).addTo(mymap);
 
+marker.bindPopup("<b>Pancake Rocks</b><br>a Location of phenomena").openPopup();
