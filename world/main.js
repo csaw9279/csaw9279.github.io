e47064dbd762b3dbb63647ce82d1bcc8438cc0cb
csaw9279 <ayhanxkosan@gmail.com>
2020-04-09 16:04:04 +0200

    js

--- world/main.js
+++ world/main.js
@@ -1,5 +1,4 @@
-let startLayer = L.tileLayer.provider("Esri.WorldTopoMap");
-
+let startLayer = L.tileLayer.provider("OpenTopoMap");
 let map = L.map("map", {
     center: [0, 0],
     zoom: 2,
@@ -11,61 +10,58 @@ let map = L.map("map", {
 let circleGroup = L.featureGroup().addTo(map);
 
 L.control.layers({
-    "OpenTopoMap": L.tileLayer.provider("OpenTopoMap"),
+    "OpenTopoMap": startLayer,
     "OpenStreetMap.Mapnik": L.tileLayer.provider("OpenStreetMap.Mapnik"),
-    "Stamen.TonerLite": L.tileLayer.provider("Stamen.TonerLite"),
     "Stamen.Watercolor": L.tileLayer.provider("Stamen.Watercolor"),
-    "Stamen.Terrain": L.tileLayer.provider("Stamen.Terrain"),
     "Stamen.TerrainBackground": L.tileLayer.provider("Stamen.TerrainBackground"),
-    "Esri.WorldStreetMap": L.tileLayer.provider("Esri.WorldStreetMap"),
-    "Esri.WorldTopoMap": startLayer,
     "Esri.WorldImagery": L.tileLayer.provider("Esri.WorldImagery"),
-    "Esri.WorldPhysical": L.tileLayer.provider("Esri.WorldPhysical"),
-    "Esri.WorldGrayCanvas": L.tileLayer.provider("Esri.WorldGrayCanvas"),
-    "CartoDB.Positron": L.tileLayer.provider("CartoDB.Positron")
 }, {
     "Thematische Darstellung": circleGroup
-}).addTo(map);
+}).addTo(map)
+
 
 let drawCircles = function () {
     let data = CONFIRMED;
     let header = CONFIRMED[0];
-    let index = header.length - 1;
+    let index = header.length - 1; 
     let topic = "bestätigte Fälle";
 
-    // Datum & THema anzeigen
-    document.querySelector("datum").innerHTML = àm ${header[index]} - $(topic)`;
+    //Datum und Thema anzeigen
+
+    document.querySelector("#datum").innerHTML = `am ${header[index]} - ${topic}`;
 
 
-    //console.log(data);
-    for (let i = 1; i < data.length; i++) {
-        let row = data[i];
-        //console.log(row[2],row[3]);
-        let reg = `${row[0]} ${row[1]}`;
-        let lat = row[2];
-        let lng = row[3];
-        let val = row[index];
-        //let mrk = L.marker([lat,lng]).addTo(map);
-        //mrk.bindPopup(`${reg}: ${val}`);
+        for (let i = 1; i < data.length; i++) {
+            let row = data[i];
+            // console.log(row[2], row[3]);
+            let reg = `${row[0]} ${row[1]}`;
+            let lat = row[2];
+            let lng = row[3];
+            let val = row[index];
+            // let mrk = L.marker([lat,lng]).addTo(map);
+            // mrk.bindPopup(`${reg}: ${val}`);
 
-        //A = r²*PI
-        //r² = A/PI
-        //r = WURZEL(A/PI)
-        let s = 0.5;
-        let r = Math.sqrt(val * s / Math.PI);
-        let circle = L.circleMarker([lat, lng], {
-            radius: r
-        }).addTo(circleGroup);
-        circle.bindPopup(`${reg}: ${val}`);
-    }
+            let s = 0.5;
+            let r = Math.sqrt(val * s / Math.PI);
+
+            let circle = L.circleMarker([lat, lng], {
+                radius: r
+            }).addTo(circleGroup);
+            circle.bindPopup(`${reg}: ${val}`);
+
+        }
+
 };
 
-document.query.selector("pulldown").onchange = function(){
+document.querySelector("#pulldown").onchange = function () {
     drawCircles();
-}
+
+};
 
 
 drawCircles();
+// drawCircles(RECOVERED);
+// drawCircles(DEATHS);
 
 
 
