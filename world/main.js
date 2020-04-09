2af24210517b233801cc7fdaca075f31d275621d
csaw9279 <ayhanxkosan@gmail.com>
2020-04-09 17:09:32 +0200

    main . js replaced

--- world/main.js
+++ world/main.js
@@ -1,4 +1,4 @@
-let startLayer = L.tileLayer.provider("OpenTopoMap")
+let startLayer = L.tileLayer.provider("Esri.WorldTopoMap");
 
 let map = L.map("map", {
     center: [30, 0],
@@ -11,65 +11,65 @@ let map = L.map("map", {
 let circleGroup = L.featureGroup().addTo(map);
 
 L.control.layers({
-    "OpenTopoMap": startLayer,
+    "OpenTopoMap": L.tileLayer.provider("OpenTopoMap"),
     "OpenStreetMap.Mapnik": L.tileLayer.provider("OpenStreetMap.Mapnik"),
-    "OpenStreetMap.BZH": L.tileLayer.provider("OpenStreetMap.BZH"),
-    "OpenStreetMap.HOT": L.tileLayer.provider("OpenStreetMap.HOT"),
-    "OpenStreetMap.DE": L.tileLayer.provider("OpenStreetMap.DE"),
-    "OpenStreetMap.France": L.tileLayer.provider("OpenStreetMap.France"),
-    "OpenStreetMap.CH": L.tileLayer.provider("OpenStreetMap.CH"),
-    "Thunderforest.SpinalMap": L.tileLayer.provider("Thunderforest.SpinalMap"),
-    "OpenMapSurfer.Roads": L.tileLayer.provider("OpenMapSurfer.Roads")
-
+    "Stamen.TonerLite": L.tileLayer.provider("Stamen.TonerLite"),
+    "Stamen.Watercolor": L.tileLayer.provider("Stamen.Watercolor"),
+    "Stamen.Terrain": L.tileLayer.provider("Stamen.Terrain"),
+    "Stamen.TerrainBackground": L.tileLayer.provider("Stamen.TerrainBackground"),
+    "Esri.WorldStreetMap": L.tileLayer.provider("Esri.WorldStreetMap"),
+    "Esri.WorldTopoMap": startLayer,
+    "Esri.WorldImagery": L.tileLayer.provider("Esri.WorldImagery"),
+    "Esri.WorldPhysical": L.tileLayer.provider("Esri.WorldPhysical"),
+    "Esri.WorldGrayCanvas": L.tileLayer.provider("Esri.WorldGrayCanvas"),
+    "CartoDB.Positron": L.tileLayer.provider("CartoDB.Positron")
 }, {
     "Thematische Darstellung": circleGroup
 }).addTo(map);
 
-
-
-//L.marker([0,0]).addTo(map);
 let drawCircles = function () {
     let data = CONFIRMED;
     let header = CONFIRMED[0];
     let index = document.querySelector("#slider").value;
-    let options =document.querySelector("#pulldown").options;
+    let options = document.querySelector("#pulldown").options;
     let value = options[options.selectedIndex].value;
     let label = options[options.selectedIndex].text;
     let color;
     //console.log(value,label,options);
 
-    if(value === "confirmed"){
+    if (value === "confirmed") {
         data = CONFIRMED;
         color = "#0074D9";
-    }else if (value === "deaths") {
+    } else if (value === "deaths") {
         data = DEATHS;
-        color ="#B10DC9";
-    }else {
+        color = "#B10DC9";
+    } else {
         data = RECOVERED;
-        color ="#2ECC40"
+        color = "#2ECC40";
     }
 
+    console.log(CONFIRMED == RECOVERED);
 
-    //Datum anzeigen
+    // Datum & Thema anzeigen anzeigen
     document.querySelector("#datum").innerHTML = `am ${header[index]} - ${label}`;
-    
+
     circleGroup.clearLayers();
 
+    //console.log(data);
     for (let i = 1; i < data.length; i++) {
         let row = data[i];
-
         //console.log(row[2],row[3]);
         let reg = `${row[0]} ${row[1]}`;
         let lat = row[2];
         let lng = row[3];
         let val = row[index];
-        // let mrk = L.marker([lat,lng]).addTo(map);
-        // mrk.bindPopup(`${reg}: ${val}`);
+        //let mrk = L.marker([lat,lng]).addTo(map);
+        //mrk.bindPopup(`${reg}: ${val}`);
 
-
-        // A = r2 * PI
-        // r = WURZEL (A/PI)
-        let s = 0.25;
+        //A = r²*PI
+        //r² = A/PI
+        //r = WURZEL(A/PI)
+        let s = 0.5;
         let r = Math.sqrt(val * s / Math.PI);
         let circle = L.circleMarker([lat, lng], {
             radius: r,
@@ -79,21 +79,18 @@ let drawCircles = function () {
     }
 };
 
-document.querySelector("#pulldown").onchange = function () {
+document.querySelector("#pulldown").onchange = function() {
     drawCircles();
-
 };
 
-let slider =document.querySelector("#slider");
-slider.min = 4; 
-slider.max = CONFIRMED[0].length -1;
+let slider = document.querySelector("#slider");
+slider.min = 4;
+slider.max = CONFIRMED[0].length - 1;
 slider.step = 1;
 slider.value = slider.max;
 
-slider.onchange = function(){
+slider.onchange = function () {
     drawCircles();
-}
+};
 
 drawCircles();
-//drawCircles(RECOVERED);
-//drawCircles(DEATHS);
