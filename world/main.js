abda6dd0eb55e27168fcd31d815c0075538f319a
csaw9279 <ayhanxkosan@gmail.com>
2020-04-09 16:51:07 +0200

    korrigiert vorlesung push colors: colors

--- world/main.js
+++ world/main.js
@@ -34,14 +34,15 @@ let drawCircles = function () {
     let options = document.querySelector("#pulldown").options;
     let value = options[options.selectedIndex].value;
     let label = options[options.selectedIndex].text;
+    let color;
     //console.log(value,label,options);
 
     if (value === "confirmed") {
         data = CONFIRMED;
-        color = "#7FDBFF";
+        color = "#0074D9";
     } else if (value === "deaths") {
         data = DEATHS;
-        color = "#111111";
+        color = "#B10DC9";
     } else {
         data = RECOVERED;
         color = "#2ECC40";
@@ -71,7 +72,7 @@ let drawCircles = function () {
         let s = 0.5;
         let r = Math.sqrt(val * s / Math.PI);
         let circle = L.circleMarker([lat, lng], {
-            radius: r
+            radius: r,
             color: color
         }).addTo(circleGroup);
         circle.bindPopup(`${reg}: ${val}`);
@@ -80,89 +81,6 @@ let drawCircles = function () {
 
 document.querySelector("#pulldown").onchange = function() {
     drawCircles();
-};
-
-drawCircles();
-
-
-
-/*let startLayer = L.tileLayer.provider("OpenTopoMap")
-let map = L.map("map", {
-    center: [30, 0],
-    zoom: 2,
-    layers: [
-        startLayer
-    ]
-});
-
-let circleGroup = L.featureGroup().addTo(map);
-
-L.control.layers({
-    "OpenTopoMap": startLayer,
-    "OpenStreetMap.Mapnik": L.tileLayer.provider("OpenStreetMap.Mapnik"),
-    // "Stadia.OSMBright": L.tileLayer.provider("Stadia.OSMBright"),
-    "Thunderforest.OpenCycleMap": L.tileLayer.provider("Thunderforest.OpenCycleMap"),
-    "CyclOSM": L.tileLayer.provider("CyclOSM"),
-    "OpenMapSurfer.Roads": L.tileLayer.provider("OpenMapSurfer.Roads")
-}, {
-    "Thematische Darstellung": circleGroup
-}).addTo(map);
-
-let drawCircles = function () {
-    let data = CONFIRMED;
-    let header = CONFIRMED[0];
-    let index = header.length - 1;
-    let options = document.querySelector("#pulldown").options;
-    let value = options[options.selectedIndex].value;
-    let label = options[options.selectedIndex].text;
-    let color;
-    // console.log(value,options);
-
-    if (value === "confirmed") {
-        data = CONFIRMED;
-        color = "blue";
-    } else if (value === "deaths") {
-        data = DEATHS;
-        color = "purple";
-    } else {
-        data = RECOVERED;
-        color = "green";
-    }
-
-    // Datum anzeigen & Thema anzeigen
-    document.querySelector("#datum").innerHTML = `am ${header[index]} - ${label}`;
-
-    circleGroup.clearLayers();
-
-    //console.log(data);
-    for (let i = 1; i < data.length; i++) {
-        let row = data[i];
-        //console.log(row[2],row[3]);
-        let reg = `${row[0]} ${row[1]}`;
-        let lat = row[2];
-        let lng = row[3];
-        let val = row[index];
-        //let mrk = L.marker([lat,lng]).addTo(map);
-        //mrk.bindPopup(`${reg}: ${val}`);
-
-        //A = r²*PI
-        //r² = A/PI
-        //r = WURZEL(A/PI)
-        let s = 0.5;
-        let r = Math.sqrt(val * s / Math.PI);
-        let circle = L.circleMarker([lat, lng], {
-            radius: r
-        }).addTo(circleGroup);
-        circle.bindPopup(`${reg}: ${val}`);
-    }
-};
-
-// drawCircles(DEATHS);
-// drawCircles(RECOVERED);
-
-document.querySelector("#pulldown").onchange = function () {
-    drawCircles();
-
 };
 
 drawCircles();
