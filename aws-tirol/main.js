aafac6c743fe18ba0f3bb8120438249e9de8b44f
csaw9279 <ayhanxkosan@gmail.com>
2020-04-23 17:51:05 +0200

    +++

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -56,28 +56,23 @@ let aws = L.geoJson.ajax(awsUrl, {
     }
 }).addTo(overlay.stations);
 
-
-
-let getColor = function (val, ramp) {
- //   console.log(val, ramp);
+let getColor = function(val, ramp) {
+    //console.log(val, ramp);
     let col = "red";
 
     for (let i = 0; i < ramp.length; i++) {
         const pair = ramp[i];
-        if (val >= pair [0]) {
+        if (val >= pair[0]) {
             break;
         } else {
-            col = pair [1];
+            col = pair[1];
         }
         //console.log(val,pair);
     }
-    
-
     return col;
 };
-let color = getColor(34,COLORS.temperature);
-console.log(color);
 
+//console.log(color);
 
 let drawTemperature = function(jsonData) {
     //console.log("aus der Funktion", jsonData);
@@ -86,10 +81,11 @@ let drawTemperature = function(jsonData) {
             return feature.properties.LT;
         },
         pointToLayer: function(feature, latlng) {
+            let color = getColor(feature.properties.LT,COLORS.temperature);
             return L.marker(latlng, {
                 title: `${feature.properties.name} (${feature.geometry.coordinates[2]}m)`,
                 icon: L.divIcon({
-                    html: `<div class="label-temperature">${feature.properties.LT.toFixed(1)}</div>`,
+                    html: `<div class="label-temperature" style="background-color:${color}">${feature.properties.LT.toFixed(1)}</div>`,
                     className: "ignore-me" // dirty hack
                 })
             })
@@ -129,6 +125,5 @@ aws.on("data:loaded", function() {
 
     overlay.temperature.addTo(map);
 
-//    console.log(COLORS);
+    //console.log(COLORS);
 });
-
