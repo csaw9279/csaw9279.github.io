d993b66c4f73cf8e4c069f82a24b5aa297a78fc7
csaw9279 <ayhanxkosan@gmail.com>
2020-04-30 11:00:20 +0200

    ?! karte weg

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -33,6 +33,7 @@ L.control.layers({
     "Temperatur (°C)": overlay.temperature,
     "Windgeschwindigkeit (km/h)": overlay.wind,
     "Relative Luftfeuchtigkeit (%)": overlay.humidity
+    
 }).addTo(map);
 
 let awsUrl = "https://aws.openweb.cc/stations";
