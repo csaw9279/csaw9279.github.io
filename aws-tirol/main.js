507a85012d54b8f410a27843d887f6735723431b
csaw9279 <ayhanxkosan@gmail.com>
2020-04-30 11:03:53 +0200

    RH = humidity

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -135,7 +135,7 @@ let drawHumidity = function (jsonData) {
             let color = getColor(feature.properties.RH, COLORS.humidity);
 
             return L.marker(latlng, {
-                title: `${feature.properties.name} (${feature.geometry.coordinates[2]}m) - ${feature.properties.HS} cm`,
+                title: `${feature.properties.name} (${feature.geometry.coordinates[2]}m) - ${feature.properties.RH} cm`,
                 icon: L.divIcon({
                     html: `<div class="label-humidity" style="background-color:${color}">${feature.properties.RH.toFixed(1)}</div>`,
                     className: "ignore-me" // dirty hack
