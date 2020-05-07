b9189a7c0d1e355ee945345d35b32410488c4cbc
csaw9279 <ayhanxkosan@gmail.com>
2020-05-07 13:08:20 +0200

    +++

--- adlerweg/einkehr.js
+++ adlerweg/einkehr.js
@@ -8,9 +8,12 @@ const EINKEHR = [
     [Etappennummer(28), "Badener Hütte", 47.081128, 47.081128],
     [Etappennummer(29), "Venedigerhaus", 47.123304, 12.451420],
     [Etappennummer(29), "Matreier Tauernhaus", 47.119034,12.496518],
-    [Etappennummer(32), "Bergeralm", 47.76567, 13.550987],
-    [Etappennummer(32), "Moaalm", 47.19475, 12.35448],
-    [Etappennummer(32), "Stüdlhütte", 47.054783,12.680916],
+    [Etappennummer(30), "Steiner Alm", ],
+    [Etappennummer(30), "Sudentendeutsche Hütte", 47.0493723,12.5762715],
+    [Etappennummer(31), "Kalser Tauernhaus", 47.0707027,12.6238444],
+    [Etappennummer(32), "Bergeralm", 47.7656702, 13.5509866],
+    [Etappennummer(32), "Moaalm", 47.1947499,12.3544804],
+    [Etappennummer(32), "Stüdlhütte", 47.0548291,12.6810885],
     [Etappennummer(33), "Glorer Hütte", 47.0310067,12.7155279],
-    [Etappennummer(33), "Luchknerhaus", 47.2613876,11.4129345]
+    [Etappennummer(33), "Lucknerhaus", 47.02081298828125,12.687911987304688]
 ];
