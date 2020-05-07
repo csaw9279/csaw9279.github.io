5bb09fd4c9b5f3f8f77e4a16fc96a4644b05f6fb
csaw9279 <ayhanxkosan@gmail.com>
2020-05-07 12:58:18 +0200

    einkehr

--- adlerweg/einkehr.js
+++ adlerweg/einkehr.js
@@ -1,15 +1,16 @@
 const EINKEHR = [
-
-    [Etappennummer(25), Orta, 47, 11],
-    [Etappennummer(26), Ortb, 47, 11],
-    [Etappennummer(27), Ortb, 47, 11],
-    [Etappennummer(28), Ortb, 47, 11],
-    [Etappennummer(29), Ortb, 47, 11],
-    [Etappennummer(30), Ortb, 47, 11],
-    [Etappennummer(31), Ortb, 47, 11],
-    [Etappennummer(32), Ortb, 47, 11],
-    [Etappennummer(33), Ortb, 47, 11],
-
-
-
+    [Etappennummer(25), "Stoanalm", 47.024944, 12.316395],
+    [Etappennummer(25), "Essener- und Rostocker Hütte", 47.054859, 12.297774],
+    [Etappennummer(25), "Johannishütte", 47.059877, 12.334337],
+    [Etappennummer(26), "Neue Sajathütte", 47.037904, 12.353436],
+    [Etappennummer(26), "Eisseehütte", 47.037904, 12.353436],
+    [Etappennummer(27), "Bonn-Matreier-Hütte", 47.038888, 12.427469],
+    [Etappennummer(28), "Badener Hütte", 47.081128, 47.081128],
+    [Etappennummer(29), "Venedigerhaus", 47.123304, 12.451420],
+    [Etappennummer(29), "Matreier Tauernhaus", 47.119034,12.496518],
+    [Etappennummer(32), "Bergeralm", 47.76567, 13.550987],
+    [Etappennummer(32), "Moaalm", 47.19475, 12.35448],
+    [Etappennummer(32), "Stüdlhütte", 47.054783,12.680916],
+    [Etappennummer(33), "Glorer Hütte", 47.0310067,12.7155279],
+    [Etappennummer(33), "Luchknerhaus", 47.2613876,11.4129345]
 ];
