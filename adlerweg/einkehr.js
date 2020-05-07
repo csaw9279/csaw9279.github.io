d28ce8bc9e8a05d2ce68e6bfc09bc2748ddbbe55
csaw9279 <ayhanxkosan@gmail.com>
2020-05-07 11:48:41 +0200

    etappen.js plus konzept

---
+++ adlerweg/einkehr.js
@@ -0,0 +1,15 @@
+const EINKEHR = [
+
+    [Etappennummer(25), Orta, 47, 11],
+    [Etappennummer(26), Ortb, 47, 11],
+    [Etappennummer(27), Ortb, 47, 11],
+    [Etappennummer(28), Ortb, 47, 11],
+    [Etappennummer(29), Ortb, 47, 11],
+    [Etappennummer(30), Ortb, 47, 11],
+    [Etappennummer(31), Ortb, 47, 11],
+    [Etappennummer(32), Ortb, 47, 11],
+    [Etappennummer(33), Ortb, 47, 11],
+
+
+
+];
